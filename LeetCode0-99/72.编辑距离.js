fcd179641f1142896b60172eea947db114e6f987
ChenMeilong <chemeilong1998@foxmail.com>
2023-06-28 10:01:31 +0800

    updata

--- LeetCode0-99/72.编辑距离.js
+++ LeetCode0-99/72.编辑距离.js
@@ -34,30 +34,50 @@
 // };
 
 //动态规划  空间可优化成m+n
-var minDistance = function(word1, word2) {
-    let m = word1.length+1
-    let n = word2.length+1
-    let dp = new Array(m).fill(-1).map(()=>new Array(n).fill(-1))
-    // 初始化第一行和第一列
-    dp[0][0] = 0 
-    for(let i = 1; i<m; i++){
-        dp[i][0] = i
-    }
-    for(let j = 1; j<n; j++){
-        dp[0][j] = j
-    }
-    for(let i = 1; i<m; i++){
-        for(let j = 1; j<n; j++){
-            if(word1[i-1]===word2[j-1]){
-                dp[i][j] =dp[i-1][j-1]
-            }else{
-                dp[i][j] = 1+Math.min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
-            }
+// var minDistance = function(word1, word2) {
+//     let m = word1.length+1
+//     let n = word2.length+1
+//     let dp = new Array(m).fill(-1).map(()=>new Array(n).fill(-1))
+//     // 初始化第一行和第一列
+//     dp[0][0] = 0 
+//     for(let i = 1; i<m; i++){
+//         dp[i][0] = i
+//     }
+//     for(let j = 1; j<n; j++){
+//         dp[0][j] = j
+//     }
+//     for(let i = 1; i<m; i++){
+//         for(let j = 1; j<n; j++){
+//             if(word1[i-1]===word2[j-1]){
+//                 dp[i][j] =dp[i-1][j-1]
+//             }else{
+//                 dp[i][j] = 1+Math.min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
+//             }
             
-        }
-    }
-    return dp[m-1][n-1]
-};
+//         }
+//     }
+//     return dp[m-1][n-1]
+// };
 
+// 空间优化后
+// var minDistance = function(word1, word2) {
+//     let  m = word1.length
+//     let  n = word2.length
+//     let dp = new Array(n+1).fill(0).map((num,index)=> index)
+//     for(let i=1;i<=m;i++){
+//         let leftTop = dp [0]
+//         dp[0] = i
+//         for(let j=1;j<=n;j++){
+//             let buff = dp[j]
+//             if(word1[i-1]===word2[j-1]){
+//                 dp[j] = leftTop
+//             }else{
+//                 dp[j] = Math.min(dp[j-1],leftTop,dp[j])+1
+//             }
+//             leftTop = buff
+//         }
+//     }
+//     return dp[n]
+// };
 
-console.log(minDistance("horse","ros"));
+console.log(minDistance("ros","horse"));
